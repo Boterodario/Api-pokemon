7a10d1c468a8db48e3cc299939d8c68eb2f032ec
Boterodario <dario.botero@hotmail.com>
2023-02-23 08:37:27 -0500

    se agregan botones NEXT Y PREVIOUS

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const d = document
 $main = d.querySelector("main"),
-$links = d.querySelector("links");
+$links = d.querySelector(".links");
 
 let pokeAPI = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=25";
 
@@ -12,7 +12,7 @@ async function loadPokemon(url){
         $template = "",
         $prevLink,
         $netxLink;
-        // console.log(json);
+         console.log(json);
 
 
      if(!res.ok) throw {status:res.status,statusText:res.statusText}
@@ -25,7 +25,7 @@ async function loadPokemon(url){
             let res = await fetch(json.results[i].url),
             pokemon = await res.json();
 
-            console.log(res,pokemon);
+            // console.log(res,pokemon);
 
             if(!res.ok) throw {status:res.status,statusText:res.statusText}
 
@@ -51,9 +51,12 @@ async function loadPokemon(url){
      }
 
 
-      $main.innerHTML = $template;
+       $main.innerHTML = $template;
+      $prevLink = json.previous ? `<a href= "${json.previous}">⏪</a>`:"";
+      $nextLink = json.next ? `<a href= "${json.next}">⏭️</a>`:"";
+      $links.innerHTML = $prevLink+ " " +$nextLink;
     } catch(err){
-        console.log(err);
+        // console.log(err);
         let message = err.statusText || "Ocurrio un error";
         $main.innerHTML = `<p>Error ${err.status}: ${message}</p>`;
 
@@ -62,4 +65,10 @@ async function loadPokemon(url){
 
 
 d.addEventListener("DOMContentLoaded", e => loadPokemon(pokeAPI));
-(pokeAPI);
+
+d.addEventListener("click",e=>{
+if(e.target.matches(".links a")){
+    e.preventDefault();
+    loadPokemon(e.target.getAttribute("href"));
+}
+});
